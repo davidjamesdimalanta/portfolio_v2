84645d16673cac0ed8cb6456472ab5ce775a10ca
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2025-03-11 21:41:35 -0400

    default to light mode

--- app/components/theme-provider.js
+++ app/components/theme-provider.js
@@ -3,5 +3,5 @@
 import { ThemeProvider } from 'next-themes'
 
 export default function ThemeProviderWrapper({ children }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>
+  return <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light">{children}</ThemeProvider>
 } 
