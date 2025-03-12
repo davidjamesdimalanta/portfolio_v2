8be4b2f8a22e3c7485290e57536d61f36ee548a2
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2025-03-12 00:15:47 -0400

    remove dead link

--- app/(casestudies)/vs-code-mobile/page.js
+++ app/(casestudies)/vs-code-mobile/page.js
@@ -168,27 +168,6 @@ export default function InnovationHubCaseStudy() {
             </svg>
             Previous: Portfolio Website
           </Link>
-          
-          <Link 
-            href="/ux-internship" 
-            className="group flex items-center px-6 py-3 bg-primary hover:bg-primary/90 text-white rounded-md transition-colors duration-300"
-          >
-            Next: UX Research Internship
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              width="24" 
-              height="24" 
-              viewBox="0 0 24 24" 
-              fill="none" 
-              stroke="currentColor" 
-              strokeWidth="2" 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              className="ml-2 group-hover:translate-x-1 transition-transform duration-300"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </Link>
         </div>
       </section>
     </div>
